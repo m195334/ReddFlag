1957bb2ad5f29f0beaa23bf683f434358a0f289c
Maddie Reynolds <m195334@usna.edu>
2018-11-23 19:12:24 -0600

    Fixed some search stuff

--- reader.py
+++ reader.py
@@ -6,59 +6,57 @@ from tika import parser
 
 
 try:
-  parsed = parser.from_file('2015-q2/2015-06/P(1)0000182.pdf')
+  parsed = parser.from_file('../2015-06/P(1)0002117.pdf')
 except:
   print("fail")
   sys.exit(0)
 
 
-adIDFind = re.compile("\s*a\s*d\s*i\s*d\s*([0-9]*)", flags = re.VERBOSE|re.IGNORECASE)
+adID = re.compile("\s*a\s*d\s*i\s*d\s*([0-9]*)", flags = re.VERBOSE|re.IGNORECASE)
 
-locationFind = re.compile("\s*location.\s*((\w|.)*)", flags = re.VERBOSE|re.IGNORECASE)
+targetedLocation = re.compile("\s*[A|a][D|d]\s*[T|t]argeting\s*[L|l]ocation[^:]*:\s*([A-Za-z\t .]+)", flags = re.VERBOSE|re.IGNORECASE)
 
-interestFind = re.compile("\s*interests.\s*((\w|,|.)*)", flags = re.VERBOSE|re.IGNORECASE)
+interest = re.compile("\s*interests.\s*((\w|,|.)*)", flags = re.VERBOSE|re.IGNORECASE)
 
-excludedFind = re.compile("\s*excluded\s*connections\s*[:|-]?\s*([A-Za-z\t .]+)", flags = re.VERBOSE|re.IGNORECASE)
+people = re.compile("\s*People\s*who\s*match:\s([A-Za-z\t .]+)*", flags = re.VERBOSE|re.IGNORECASE)
 
-ageFind = re.compile("\s*age\s*[:|-]\s*([0-9]*\s*[+|-]?\s*[0-9]*[+|-]?)", flags = re.VERBOSE|re.IGNORECASE)
+excluded = re.compile("\s*excluded\s*connections\s*[:|-]?\s*([A-Za-z\t .]+)", flags = re.VERBOSE|re.IGNORECASE)
 
-languageFind = re.compile("\s*language\s*[:|-]?\s*([A-Za-z()\t .]+)", flags = re.VERBOSE|re.IGNORECASE)
+age = re.compile("\s*age\s*[:|-]\s*([0-9]*\s*[+|-]?\s*[0-9]*[+|-]?)", flags = re.VERBOSE|re.IGNORECASE)
 
-placementFind = re.compile("\s*placements\s*[:|-]?\s*([A-Za-z\t .]+)", flags = re.VERBOSE|re.IGNORECASE)
+language = re.compile("\s*language\s*[:|-]?\s*([A-Za-z()\t .]+)", flags = re.VERBOSE|re.IGNORECASE)
 
-URLFind = re.compile("\s*(http[\S]*)", flags = re.VERBOSE|re.IGNORECASE)
+placement = re.compile("\s*placements\s*[:|-]?\s*([A-Za-z\t .]+)", flags = re.VERBOSE|re.IGNORECASE)
 
-timeFind = re.compile("[0-9]+/[0-9]+/[0-9]+\s*[\S]*\s*[\S]*\s*[\S]*", flags = re.VERBOSE|re.IGNORECASE)
+URL = re.compile("\s*(http[\S]*)", flags = re.VERBOSE|re.IGNORECASE)
 
+time = re.compile("[0-9]+/[0-9]+/[0-9]+\s*[\S]*\s*[\S]*\s*[\S]*", flags = re.VERBOSE|re.IGNORECASE)
 
-adID = adIDFind.findall(parsed["content"])
-location = locationFind.findall(parsed["content"])
-interest = interestFind.findall(parsed["content"])
-excluded = excludedFind.findall(parsed["content"])
-age = ageFind.findall(parsed["content"])
-language = languageFind.findall(parsed["content"])
-placement = placementFind.findall(parsed["content"])
-URL = URLFind.findall(parsed["content"])
-time = timeFind.findall(parsed["content"])
+impressions = re.compile("ad\s*impressions\s*:?\s*([0-9]*)", flags = re.VERBOSE|re.IGNORECASE)
+
+clicks = re.compile("ad\s*clicks\s*:?\s*([0-9]*)", flags = re.VERBOSE|re.IGNORECASE)
+
+spend = re.compile("ad\s*spend\s*:?\s*([A-Za-z0-9()\t .]+)", flags = re.VERBOSE|re.IGNORECASE)
+
+def tryFind(finder, searchName, f):
+  x = " ".join(finder.findall(parsed["content"]))
+  if(x != ""):
+    f.write(searchName + x + "\n")
+  else:
+    f.write(searchName + " No Value\n")
 
 f = open("inClassExample.txt", "w")
-f.write("adID: " + adID[0] + "\n")
-f.write("Location: " + location[0][0] + "\n")
-f.write("Interest Areas: " + interest[0] + "\n")
-f.write("Excluded Targets: " + excluded[0] + "\n")
-f.write("Age Targeted: " + age[0] + "\n")
-f.write("Language of Ad: " + language[0] + "\n")
-f.write("Placement of Ad: " + placement[0] + "\n")
-f.write("URL: " + URL[0] + "\n")
-f.write("Time Post: " + time[0] + "\n")
+tryFind(adID, "ad ID: ", f)
+tryFind(targetedLocation, "Targeted Locations: ", f)
+tryFind(interest, "interest: ", f)
+tryFind(people, "People Who Match: ", f)
+tryFind(excluded, "Excluded: ", f)
+tryFind(age, "Age: ", f)
+tryFind(language, "Language: ", f)
+tryFind(placement, "Placement: ", f)
+tryFind(URL, "URL: ", f)
+tryFind(time, "Time: ", f)
+tryFind(impressions, "Ad Impressions: ", f)
+tryFind(clicks, "Ad Clicks: ", f)
+tryFind(spend, "Ad Spend: ", f)
 f.close()
-
-f = open(adID[0] + "metaData.txt", "w")
-for x in parsed["metadata"]:
-  f.write(x)
-f.write("\n")
-f.close()
-
-
-
-
