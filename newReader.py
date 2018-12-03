84565bc0471c59e360d1ff9403d741df3b364132
Maddie Reynolds <m195334@usna.edu>
2018-12-02 22:29:59 -0600

    udpate

--- newReader.py
+++ newReader.py
@@ -5,26 +5,26 @@ import re
 from tika import parser
 
 
-adID = re.compile("\s*a\s*d\s*i\s*d\s*([0-9]*)", flags = re.VERBOSE|re.IGNORECASE)
-targetedLocation = re.compile("\s*[A|a][D|d]\s*[T|t]argeting\s*[L|l]ocation[^:]*:\s*([A-Za-z\t .]+)", flags = re.VERBOSE|re.IGNORECASE)
-interest = re.compile("\s*interests.\s*((\w|,|.)*)", flags = re.VERBOSE|re.IGNORECASE)
-people = re.compile("\s*People\s*who\s*match:\s([A-Za-z\t .]+)*", flags = re.VERBOSE|re.IGNORECASE)
-excluded = re.compile("\s*excluded\s*connections\s*[:|-]?\s*([A-Za-z\t .]+)", flags = re.VERBOSE|re.IGNORECASE)
-age = re.compile("\s*age\s*[:|-]\s*([0-9]*\s*[+|-]?\s*[0-9]*[+|-]?)", flags = re.VERBOSE|re.IGNORECASE)
-language = re.compile("\s*language\s*[:|-]?\s*([A-Za-z()\t .]+)", flags = re.VERBOSE|re.IGNORECASE)
-placement = re.compile("\s*placements\s*[:|-]?\s*([A-Za-z\t .]+)", flags = re.VERBOSE|re.IGNORECASE)
-URL = re.compile("\s*(http[\S]*)", flags = re.VERBOSE|re.IGNORECASE)
-time = re.compile("\s*Creation\s*Date\s*[0-9]+/[0-9]+/[0-9]+\s*[\S]*\s*[\S]*\s*[\S]*", flags = re.VERBOSE|re.IGNORECASE)
-timeEnd = re.compile("\s*End\s*Date\s*[0-9]+/[0-9]+/[0-9]+\s*[\S]*\s*[\S]*\s*[\S]*", flags = re.VERBOSE|re.IGNORECASE)
-impressions = re.compile("ad\s*impressions\s*:?\s*([0-9]*)", flags = re.VERBOSE|re.IGNORECASE)
-clicks = re.compile("ad\s*clicks\s*:?\s*([0-9]*)", flags = re.VERBOSE|re.IGNORECASE)
-spend = re.compile("ad\s*spend\s*:?\s*([A-Za-z0-9()\t .]+)", flags = re.VERBOSE|re.IGNORECASE)
-redactions = re.compile("redactions", flags = re.VERBOSE|re.IGNORECASE)
-houseComm = re.compile("Select\s*Committee", flags = re.VERBOSE|re.IGNORECASE)
+adID = "\s*a\s*d\s*i\s*d\s*([0-9]*)"
+targetedLocation = "\s*[A|a][D|d]\s*[T|t]argeting\s*[L|l]ocation[^:]*:\s*([A-Za-z\t .]+)"
+interest = "\s*interests.\s*((\w|,|.)*)"
+people = "\s*People\s*who\s*match:\s([A-Za-z\t .]+)*"
+excluded = "\s*excluded\s*connections\s*[:|-]?\s*([A-Za-z\t .]+)"
+age = "\s*age\s*[:|-]\s*([0-9]*\s*[+|-]?\s*[0-9]*[+|-]?)"
+language = "\s*language\s*[:|-]?\s*([A-Za-z()\t .]+)"
+placement = "\s*placements\s*[:|-]?\s*([A-Za-z\t .]+)"
+URL = "\s*(http[\S]*)"
+time = "\s*Creation\s*Date\s*[0-9]+/[0-9]+/[0-9]+\s*[\S]*\s*[\S]*\s*[\S]*"
+timeEnd = "\s*End\s*Date\s*[0-9]+/[0-9]+/[0-9]+\s*[\S]*\s*[\S]*\s*[\S]*"
+impressions = "ad\s*impressions\s*:?\s*([0-9]*)"
+clicks = "ad\s*clicks\s*:?\s*([0-9]*)"
+spend = "ad\s*spend\s*:?\s*([A-Za-z0-9()\t .]+)"
+redactions = "redactions"
+houseComm = "Select\s*Committee"
 
 def getFileData(fileName):
   try:
-    parsed = parser.from_file('../2015-06/P(1)0002117.pdf')
+    parsed = parser.from_file('../2015-06/P(1)0002262.pdf')
     parsed = parsed['content']
     parsed = parsed.splitlines()
     parsed = map(str, parsed)
@@ -35,13 +35,16 @@ def getFileData(fileName):
     sys.exit(0)
 
 def tryFind(parsed, finder, searchName, f):
-  x = [i for i, item in enumerate(parsed) if re.findall(finder, item)]
-  if len(x) == 1:
-    x = x[0]
-    f.write(parsed[x] + "\n")
-    parsed.pop(x)
-  else:
-    f.write(searchName + " No Value\n")
+  for item in enumerate(parsed):
+    m = re.search(finder, item[1], flags = re.VERBOSE|re.IGNORECASE)
+    if m:
+      match = m.groups(0)[0]
+      f.write(searchName + match + "\n")
+      parsed.pop(item[0])
+      return parsed
+    else:
+      continue
+  f.write(searchName + " No Value\n")
   return parsed
 
 def findRemove(parsed, finder):
@@ -50,25 +53,25 @@ def findRemove(parsed, finder):
     parsed.pop(y)
   return parsed
 
-parsed = getFileData("none")
-adID = ''.join(filter(adID.findall, parsed))
-f = open(adID + ".txt", "w")
-parsed = tryFind(parsed, adID, "Ad ID ", f)
-parsed = tryFind(parsed, targetedLocation, "Targeted Locations ", f)
-parsed = tryFind(parsed, interest, "Interest ", f)
-parsed = tryFind(parsed, people, "People Who Match ", f)
-parsed = tryFind(parsed, excluded, "Excluded ", f)
-parsed = tryFind(parsed, age, "Age ", f)
-parsed = tryFind(parsed, language, "Language ", f)
-parsed = tryFind(parsed, placement, "Placement ", f)
-parsed = tryFind(parsed, URL, "URL ", f)
-parsed = tryFind(parsed, time, "Ad Creation Date ", f)
-parsed = tryFind(parsed, timeEnd, "Ad End Date ", f)
-parsed = tryFind(parsed, impressions, "Ad Impressions ", f)
-parsed = tryFind(parsed, clicks, "Ad Clicks ", f)
-parsed = tryFind(parsed, spend, "Ad Spend ", f)
+
+filename = "none"
+parsed = getFileData(filename)
+f = open("readAds/" + filename, "w")
+parsed = tryFind(parsed, adID, "Ad ID: ", f)
+parsed = tryFind(parsed, targetedLocation, "Targeted Locations: ", f)
+parsed = tryFind(parsed, interest, "Interest: ", f)
+parsed = tryFind(parsed, people, "People Who Match: ", f)
+parsed = tryFind(parsed, excluded, "Excluded: ", f)
+parsed = tryFind(parsed, age, "Age: ", f)
+parsed = tryFind(parsed, language, "Language: ", f)
+parsed = tryFind(parsed, placement, "Placement: ", f)
+parsed = tryFind(parsed, URL, "URL: ", f)
+parsed = tryFind(parsed, time, "Ad Creation Date: ", f)
+parsed = tryFind(parsed, timeEnd, "Ad End Date: ", f)
+parsed = tryFind(parsed, impressions, "Ad Impressions: ", f)
+parsed = tryFind(parsed, clicks, "Ad Clicks: ", f)
+parsed = tryFind(parsed, spend, "Ad Spend: ", f)
 parsed = findRemove(parsed, redactions)
 parsed = findRemove(parsed, houseComm)
-
 f.write(" ".join(parsed) + "\n")
 f.close()
